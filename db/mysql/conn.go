f9211472afeaa6606b55b7d65bf68f928d1218e7
lexingsen <lexingsen@163.com>
2021-08-02 22:36:20 +0800

    fixbug 初始化全局变量db

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -12,19 +12,20 @@ import (
 var db *sql.DB
 
 func init() {
-	db, err := sql.Open("mysql", "root:111111@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	dbx, err := sql.Open("mysql", "root:111111@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
 	if err != nil {
 		fmt.Println("Failed to connet to the mysql, err:" + err.Error())
 		os.Exit(1)
 	}
 	log.Printf("mysql is opened!")
-	db.SetMaxOpenConns(1000)
-	err = db.Ping()
+	dbx.SetMaxOpenConns(1000)
+	err = dbx.Ping()
 	if err != nil {
 		fmt.Println("Failed to connect to mysql, err:" + err.Error())
 		os.Exit(1)
 	}
 	log.Printf("mysql ping is pong!")
+	db = dbx
 }
 
 func DBConn() *sql.DB {
